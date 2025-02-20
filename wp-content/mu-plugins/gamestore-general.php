03f8dabfdf5fc887d149f167e1ec02e4ee3872f6
Yulia Voloboy <mavlyu@yandex.ru>
2025-02-20 11:12:32 +0300

    Добавлен плагин gamestore-general

---
+++ wp-content/mu-plugins/gamestore-general.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Gamestore General
+ * Description: Core Code for Gamestore.
+ * Version: 1.0
+ * Author: Yulia Voloboy
+ * Author URI: https://yvoloboy.ru
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
