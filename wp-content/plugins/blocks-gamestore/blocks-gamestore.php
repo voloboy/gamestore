c688645e50a80d9a61fe52131338ebf39a830763
Yulia Voloboy <mavlyu@yandex.ru>
2025-02-20 18:48:20 +0300

    Сгенерирован плагин блоков

---
+++ wp-content/plugins/blocks-gamestore/blocks-gamestore.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Blocks Gamestore
+ * Description:       Example block scaffolded with Create Block tool.
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       blocks-gamestore
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_blocks_gamestore_block_init() {
+	register_block_type( __DIR__ . '/build/blocks-gamestore' );
+}
+add_action( 'init', 'create_block_blocks_gamestore_block_init' );
---
+++ wp-content/plugins/blocks-gamestore/build/blocks-gamestore/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '46482af295d695b4d1f3');
